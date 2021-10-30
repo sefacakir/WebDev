2b9e3cc684ff531d8e377174d9d74cb3d34ecb4d
sefacakir <cakirsefa72@gmail.com>
2021-10-30 23:27:30 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/BookStoreContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class BookStoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BookStore;Trusted_Connection=true");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+        //public DbSet<OperationClaim> OperationClaims { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Business.Abstract;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            return Ok(_bookService.GetAll());
+        }
+    }
+}
